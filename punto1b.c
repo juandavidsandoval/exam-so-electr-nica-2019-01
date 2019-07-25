20aa130b70a0f39e045bb960e1af04b5db5234c3
Juan David <juan.david.sandoval@correounivalle.edu.co>
2019-07-25 16:18:50 -0500

    Subiendo archivos del parcial

---
+++ punto1b.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "common_threads.h"
+
+#include <time.h>
+#include <pthread.h>
+
+volatile int counter = 0; 
+int loops;
+pthread_mutex_t puerta;
+void *crear_puerta(){
+	pthread_mutex_init(&puerta,NULL);
+}
+void *cerrar_puerta (){
+	pthread_mutex_lock(&puerta);
+}
+void *abrir_puerta (){
+	pthread_mutex_unlock(&puerta);
+}
+void *destruir_puerta (){
+	pthread_mutex_destroy(&puerta);
+}
+void *worker(void *arg) {
+    
+    int i;
+
+
+    for (i = 0; i < loops; i++) {
+	cerrar_puerta();
+	counter++;
+	abrir_puerta();
+    }
+
+   
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    clock_t time1, time2;
+    double secs;
+   time1 = clock();
+
+
+
+    if (argc != 2) {
+	fprintf(stderr, "usage: threads <loops>\n");
+	exit(1);
+    return 0;
+
+
+    }
+    crear_puerta();
+    loops = atoi(argv[1]);
+    pthread_t p1, p2;
+    printf("Initial value : %d\n", counter);
+    Pthread_create(&p1, NULL, worker, NULL);
+    Pthread_create(&p2, NULL, worker, NULL);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value   : %d\n", counter);
+    time2 =clock();
+    secs = (double)(time2 - time1) / CLOCKS_PER_SEC;
+    printf(" el tiempo es: %f\n", secs);
+
+    return 0;
+
+
+    destruir_puerta();
+
+
+
+
+}
+
+
--- punto3.c
+++ punto3.c
@@ -1,18 +1,21 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 int main(int argc, char**argv){
-   pid_t pid,pid2;
-   int status;
-	printf("*********Wait() para esperar el hijo***********\n");
-   pid = fork();
-   if (pid != 0){
-     pid2 = wait(&status);
-     printf("soy el padre y termine mi proceso\n");
-  } else{
-    printf("soy el hijo y termine mi proceso\n");
-  }
-  
-  return 0;
+	pid_t pid,pid2;
+   	int status;
+
+        printf("********* Waitpid() para esperar el hijo ***********\n");
+   	pid2 = fork();
+   	if (pid2 == 0){
+     		pid = waitpid(pid2,&status,0);
+     		printf("Soy el hijo y terminé mi proceso\n");
+  	} else {
+    		printf("Soy el padre y terminé mi proceso\n");
+  	}
+
+	return 0;
 }
+
