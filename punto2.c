987705f8185b40ce67ce30677793412456ef60e0
juandavidsandoval <50960608+juandavidsandoval@users.noreply.github.com>
2019-07-25 18:34:05 -0500

    Subiendo archivos del parcial

--- punto2.c
+++ punto2.c
@@ -4,14 +4,13 @@
 #include <stdlib.h>
 #include "common.h"
 #include "common_threads.h"
-#include <time.h>
-#include <pthread.h>
-float x=1250;
-float y= 250;
-float z= 25;
 
-float v;
-float resul=0;
+
+float x = 1250;
+float y = 350;
+float z = 25;
+float v = 0 ;
+float resultado = 0;
 
 
 
@@ -30,36 +29,37 @@ void *destruir_puerta (){
 	pthread_mutex_destroy(&puerta);
 }
 
-void *worker() {
+void *worker23() {
     cerrar_puerta();
-    
     v = z/y;
-    
-    
     abrir_puerta();
-    return NULL;
+    return 0;
 }
 
 void *worker1() {
-      cerrar_puerta();
-      resul = x * v;
+     cerrar_puerta();
+     float v;
+     resultado = x * v;
 
-      abrir_puerta();   
-      return(NULL);
+     abrir_puerta();   
+     return(0);
 }
 
 
 
 int main(int argc, char *argv[]) {
 
-    crear_puerta();
+
     pthread_t p1, p2;
-    Pthread_create(&p1, NULL, worker, NULL);
-    Pthread_create(&p2, NULL, worker1, NULL);
+    crear_puerta();
+    
+    Pthread_create(&p1, NULL, worker1, NULL);
+    Pthread_create(&p2, NULL, worker23, NULL);
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
-    printf ("Initial value : %f\n", resul);
+    printf ("RESULTADO DE OPERACIÓN : %f\n", resultado);
 
+    printf ("Resultado de V: %f\n", v);
     return 0;
 
     destruir_puerta();
